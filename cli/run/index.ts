b89862f697d4d7ca8956e64d5bbd9942d77c895c
Van Nguyen <van@ebex.com.au>
2019-10-19 16:13:56 +1100

    Make multiple configs build in parallel

--- cli/run/index.ts
+++ cli/run/index.ts
@@ -97,21 +97,17 @@ function execute(configFile: string, configs: GenericConfigObject[], command: an
 	if (command.watch) {
 		watch(configFile, configs, command, command.silent);
 	} else {
-		let promise = Promise.resolve();
-		for (const config of configs) {
-			promise = promise.then(() => {
-				const warnings = batchWarnings();
-				const { inputOptions, outputOptions, optionError } = mergeOptions({
-					command,
-					config,
-					defaultOnWarnHandler: warnings.add
-				});
-
-				if (optionError)
-					(inputOptions.onwarn as WarningHandler)({ code: 'UNKNOWN_OPTION', message: optionError });
-				return build(inputOptions, outputOptions, warnings, command.silent);
+		return Promise.all(configs.map(config => {
+			const warnings = batchWarnings();
+			const { inputOptions, outputOptions, optionError } = mergeOptions({
+				command,
+				config,
+				defaultOnWarnHandler: warnings.add
 			});
-		}
-		return promise;
+
+			if (optionError)
+				(inputOptions.onwarn as WarningHandler)({ code: 'UNKNOWN_OPTION', message: optionError });
+			return build(inputOptions, outputOptions, warnings, command.silent, true);
+		}));
 	}
 }
